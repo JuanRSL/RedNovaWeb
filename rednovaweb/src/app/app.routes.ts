2ff8cfd393307ba92195954af437b61db7a6de23
damoelrrr <danielfeliperodriguezangulo@gmail.com>
2026-05-27 20:33:23 -0500

    resolver conflictos de git y configuracion de arranque

--- rednovaweb/src/app/app.routes.ts
+++ rednovaweb/src/app/app.routes.ts
@@ -9,23 +9,17 @@ export const routes: Routes = [
     ]
   },
   {
-    path: '',
-    loadComponent: () => import('./layout/main-layout/main-layout.component').then(m => m.MainLayoutComponent),
+    path: 'posts',
     children: [
-      {
-        path: 'posts',
-        children: [
-          { path: '', loadComponent: () => import('./features/posts/post-list/post-list.component').then(m => m.PostListComponent) },
-          { path: 'new', loadComponent: () => import('./features/posts/post-form/post-form.component').then(m => m.PostFormComponent) },
-          { path: ':id', loadComponent: () => import('./features/posts/post-detail/post-detail.component').then(m => m.PostDetailComponent) }
-        ]
-      },
-      {
-        path: 'profile',
-        loadComponent: () => import('./features/profile/profile.component').then(m => m.ProfileComponent)
-      },
-      { path: '', redirectTo: 'posts', pathMatch: 'full' }
+      { path: '', loadComponent: () => import('./features/posts/post-list/post-list.component').then(m => m.PostListComponent) },
+      { path: 'new', loadComponent: () => import('./features/posts/post-form/post-form.component').then(m => m.PostFormComponent) },
+      { path: ':id', loadComponent: () => import('./features/posts/post-detail/post-detail.component').then(m => m.PostDetailComponent) }
     ]
   },
-  { path: '**', redirectTo: 'posts' }
+  {
+    path: 'profile',
+    loadComponent: () => import('./features/profile/profile.component').then(m => m.ProfileComponent)
+  },
+  { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'auth/login' }
 ];
